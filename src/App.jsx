3a22b3168a6d883c03a5eec8e2ab21161d125f82
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-18 19:36:22 +0100

    for managing the classes in the application

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LoginPage from './components/LoginPage'
+import DashBoard from "./components/DashBoard.jsx";
+
+function App() {
+
+  return (
+    <>
+       <LoginPage/>
+       <DashBoard/>
+    </>
+  )
+}
+
+export default App
